23387edec9ea9c7ed073273f63882d3e46469f27
Nick Rau <nickrau3@gmail.com>
2020-06-09 00:14:13 +0200

    Clientsocket changed

--- ClientSocket/ClientSocketConnector.cs
+++ ClientSocket/ClientSocketConnector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
 using System.Net.Sockets;
@@ -9,219 +10,59 @@ using System.Threading.Tasks;
 
 namespace ClientSocket
 {
-    public class ClientSocketConnector
+    public static class ClientSocketConnector
     {
-        private int socketport;
-        private IPAddress serverip;
-        private bool socketIsConnected = true;
-        private Socket client;
-
-
-        // ManualResetEvent instances signal completion.
-        private static ManualResetEvent connectDone =
-            new ManualResetEvent(false);
-        private static ManualResetEvent sendDone =
-            new ManualResetEvent(false);
-        private static ManualResetEvent receiveDone =
-            new ManualResetEvent(false);
+        private static int socketport;
+        private static IPAddress serverip;
+        private static TcpClient server;
+        private static NetworkStream stream;
 
         // The response from the remote device.
         private static String response = String.Empty;
         // Boolean to check if there is a full responcemessage
         private static bool fullMessageReceived = false;
         public static List<String> responseQueue = new List<string>();
+        public static bool haveToListen = true;
 
-        public Socket Client { get => client; set => client = value; }
-
-        public ClientSocketConnector(string _ipaddress, int _socketport) 
+        public static void PrepareConnector(string ipaddress, int portnumber)
         {
-            serverip = IPAddress.Parse(_ipaddress);
-            socketport = _socketport;
+            server = new TcpClient(ipaddress, portnumber);
+            stream = server.GetStream();
+            StartListening();
+
         }
 
-        public string PrepareSendMessage(string message)
+        public static async Task StartListening()
         {
-            return message + "<EOF>";
-        }
-
-        public void StopCLient()
-        {
-            socketIsConnected = false;
-        }
-
-        public async Task StartClient() 
-        {
-            // Connect to remote device
-            try
+            await Task.Run(() =>
             {
-                // Create remote endpoint for the socket. Where you want to connect to.
-                IPEndPoint remoteEP = new IPEndPoint(serverip,socketport);
-
-                IPAddress clientIpAddress;
-
-                // Get own ipaddress
-                using (Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+                StreamReader sr = new StreamReader(stream);
+                while (haveToListen)
                 {
-                    sock.Connect("8.8.8.8", 65530);
-                    IPEndPoint endPoint = sock.LocalEndPoint as IPEndPoint;
-                    clientIpAddress = endPoint.Address;
+                    string response = sr.ReadLine();
+                    Console.WriteLine(response);
                 }
-
-                // Create a TCP/IP socket. Make sure you use the same parameters as the server.
-                client = new Socket(clientIpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect to remote endpoint
-                client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback), client);
-                connectDone.WaitOne();
-
-                // Send test data to remote device
-                Send(client, PrepareSendMessage("{'connectionType':'connectionTest'}"));
-
-                await Task.Run(() =>
-                {
-                    while (true)
-                    {
-                        // Receive the response to the remote device.
-                        Receive(client);
-                        receiveDone.WaitOne();
-                    }
-                });
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("STARTSOCKET\n", e.ToString());
-            }
+            });
         }
 
-        private static void ConnectCallback(IAsyncResult ar)
+        public static void StopListening()
         {
-            try
-            {
-                // Receieve the socket from the state object.
-                Socket client = (Socket)ar.AsyncState;
-
-                // Complete the connection.
-                client.EndConnect(ar);
-
-                Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
-
-                // Signal that the connection has been made.
-                connectDone.Set();
-            } catch (Exception e)
-            {
-                Console.WriteLine("CONNECTCALLBACK\n", e.ToString());
-            }
+            haveToListen = false;
+            stream.Close();
+            server.Close();
         }
 
-        private static void Receive(Socket client)
+        public static void Send(string data)
         {
-            try
-            {
-                // Create state object.
-                StateObject state = new StateObject();
-                state.workSocket = client;
-
-                // Begin receiving the data from the remote device.
-                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
-            } catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            int bytecount = Encoding.ASCII.GetByteCount(data + 1);
+            byte[] sendData = new byte[bytecount];
+            sendData = Encoding.ASCII.GetBytes(data);
+            stream.Write(sendData, 0, sendData.Length);
         }
 
-        private static void ReceiveCallback(IAsyncResult ar)
-        {
-            Console.WriteLine("Recieving a callback");
-            try
-            {
-                // Retrieve the state object and the client socket
-                // from the asynchronous state object.  
-                StateObject state = (StateObject)ar.AsyncState;
-                Socket client = state.workSocket;
-
-                // Read data from the remote device.  
-                int bytesRead = client.EndReceive(ar);
-
-                if (bytesRead > 0)
-                {
-
-                    Console.WriteLine("Reading...");
-                    // There might be more data, so store the data so far.
-                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
-                    string content = state.sb.ToString();
-                    if(content.IndexOf("<EOF>") > -1)
-                    {
-                        String stringContent = content.Substring(0, content.Length - 5);
-                        Console.WriteLine("Done reading...");
-
-                        // All the data has arrived, so let's put it in response.
-                        if (state.sb.Length > 1)
-                        {
-                            response = state.sb.ToString();
-                            responseQueue.Add(response);
-                        }
-                        // Signal that all bytes have been received.
-                        receiveDone.Set();
-                        fullMessageReceived = true;
-                    }
-                    else
-                    {
-                        // Get the rest of the data.
-                        client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
-                    }
-
-                }
-
-            } catch (Exception e)
-            {
-                Console.WriteLine("RECEIVECALLBACK\n", e.ToString());
-            }
-        }
-
-        public static void Send(Socket sockclient, String data)
-        {
-            // Convert the string data to byte data using ASCII encoding.  
-            byte[] byteData = Encoding.ASCII.GetBytes(data);
-            Console.WriteLine(sockclient.AddressFamily);
-            // Begin sending the data to the remote device.  
-            sockclient.BeginSend(byteData, 0, byteData.Length, 0,
-                new AsyncCallback(SendCallback), sockclient);
-        }
-
-        private static void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.  
-                Socket client = (Socket)ar.AsyncState;
-
-                // Complete sending the data to the remote device.  
-                int bytesSent = client.EndSend(ar);
-                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
-
-                // Signal that all bytes have been sent.  
-                sendDone.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("SENDCALLBACK\n", e.ToString());
-            }
-        }
+        
 
     }
 
-    // State object for receiving data from remote device.
-    public class StateObject
-    {
-        // Client socket.
-        public Socket workSocket = null;
-        // Size of receive buffer.
-        public const int BufferSize = 256;
-        // Receive buffer.
-        public byte[] buffer = new byte[BufferSize];
-        // Received data string.
-        public StringBuilder sb = new StringBuilder();
-    }
 
 }
--- ClientSocket/ISocket.cs
+++ ClientSocket/ISocket.cs
@@ -5,13 +5,10 @@ using ClientSocket;
 
 public interface ISocket
 {
-    private static ClientSocketConnector clientSocket;
-
     public static void StartSocket(string ipaddress, int portnumber)
     {
-        clientSocket = new ClientSocketConnector(ipaddress, portnumber);
+        ClientSocketConnector.PrepareConnector("192.168.2.8", 34000);
         ResponseListener();
-        clientSocket.StartClient();
 
     }
 
@@ -34,28 +31,51 @@ public interface ISocket
     }
     static void login(string username, string password) 
     {
-        ClientSocketConnector.Send(clientSocket.Client, clientSocket.PrepareSendMessage("{'connectionType':'login', 'username': '" + username + "', 'password':'" + password + "'}"));
+        ClientSocketConnector.Send("{'connectionType':'login', 'username': '" + username + "', 'password':'" + password + "'}");
     }
 
-    static void register(string username, string password) { }
+    static void register(string username, string password) 
+    {
+        ClientSocketConnector.Send("{'connectionType':'register', 'username': '" + username + "', 'password':'" + password + "'}");
+    }
 
-    static void gamecreate(string username, string password) { }
+    static void gamecreate(string username, string password) 
+    {
+        ClientSocketConnector.Send("{'connectionType':'gamecreate', 'username': '" + username + "', 'password':'" + password + "'}");
+    }
 
-    static void gameaccess(string username) { }
+    static void gameaccess(string username) 
+    {
+        ClientSocketConnector.Send("{'connectionType':'gameaccess', 'username': '" + username + "'}");
+    }
 
-    static void roomenter(int gameid, int roomnum) { }
+    static void roomenter(int gameid, int roomnum)
+    {
+        ClientSocketConnector.Send("{'connectionType':'roomenter', 'gameid': '" + gameid + "', 'roomnum':'" + roomnum + "'}");
+    }
 
-    static void hiderhidden(int gameid) { }
+    static void hiderhidden(int gameid)
+    {
+        ClientSocketConnector.Send("{'connectionType':'hiderhidden', 'gameid': '" + gameid + "'}");
+    }
 
-    static void specialpower(int gameid, int roomnum) { }
+    static void specialpower(int gameid, int roomnum)
+    {
+        ClientSocketConnector.Send("{'connectionType':'specialpower', 'gameid': '" + gameid + "', 'roomnum':'" + roomnum + "'}");
+    }
 
-    static void seekerdone(int gameid) { }
+    static void seekerdone(int gameid)
+    {
+        ClientSocketConnector.Send("{'connectionType':'seekerdone', 'gameid': '" + gameid + "'}");
+    }
 
-    static void changeplayertype(int gameid) { }
+    static void changeplayertype(int gameid)
+    {
+        ClientSocketConnector.Send("{'connectionType':'changeplayertype', 'gameid': '" + gameid + "'}");
+    }
 
     static void testMessage()
     {
-        Console.WriteLine(clientSocket.PrepareSendMessage("{'connectionType':'connectionTest'}"));
-        ClientSocketConnector.Send(clientSocket.Client, clientSocket.PrepareSendMessage("{'connectionType':'connectionTest'}"));
+        ClientSocketConnector.Send("{'connectionType':'connectionTest'}");
     }
 }
